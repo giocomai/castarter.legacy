8b7c39316dde904385c91ce2df8e2ed93d2b134c
Giorgio <giorgiocomai@gmail.com>
2015-06-05 12:08:17 +0100

    Adding new documentation.

--- R/archiveFolders.R
+++ R/archiveFolders.R
@@ -1,5 +1,15 @@
-
-ArchiveFolders <- function(nameOfProject, nameOfWebsite) {
+#' Archives Html, Txt, and IndexHtml folders in .tar.gz files. 
+#'
+#' It archives  Html, Txt, and IndexHtml folders in .tar.gz files, and stores them in a dated sub-folder within the Archives sub-folder. 
+#' @param nameOfProject Name of 'castarter' project. Must correspond to the name of a folder in the current working directory. 
+#' @param nameOfWebsite Name of a website included in a 'castarter' project. Must correspond to the name of a sub-folder of the project folder.
+#' @param removeArchivedFolders If equal to TRUE, it removes the folders and the original html and txt files from the hard disk, after having archived them. 
+#' @export
+#' @section Warning:
+#' If the option removeArchivedFolders is enabled, the function actually deletes files from the hard disk. Files are removed only after they have been successfully stored in compressed .tar.gz file, but it is recommended to backup valuable data before enabling this option.
+#' @examples
+#' ArchiveFolders(nameOfProject, nameOfWebsite, removeArchivedFolders = FALSE))
+ArchiveFolders <- function(nameOfProject, nameOfWebsite, removeArchivedFolders = FALSE) {
     if (!file.exists(file.path(nameOfProject, nameOfWebsite, "Archives"))) {
         dir.create(file.path(nameOfProject, nameOfWebsite, "Archives"))
     }
--- R/preliminary.R
+++ R/preliminary.R
@@ -1,6 +1,6 @@
-#' A function that loads all needed packages
+#' Loads all packages needed for 'castarter'
 #'
-#' It needs to be deleted
+#' It needs to be deleted.
 #' @keywords toBeRemoved
 #' @export
 #' @examples
@@ -13,6 +13,13 @@ LoadRequiredPackages <- function() {
         "slam", "zoo", "RGtk2Extras", "scales", "reshape2", "gridExtra", "RColorBrewer", "mgcv")
 }
 
+#' Creates the folder structure needed by 'castarter'.
+#'
+#' It needs to be deleted.
+#' @keywords toBeRemoved
+#' @export
+#' @examples
+#' CreateFolderStructure(nameOfProject, nameOfWebsite)
 CreateFolderStructure <- function(nameOfProject, nameOfWebsite) {
     if (!file.exists(file.path(nameOfProject))) {
         dir.create(file.path(nameOfProject))
@@ -40,6 +47,14 @@ CreateFolderStructure <- function(nameOfProject, nameOfWebsite) {
     }
 }
 
+#' Loads last saved workspace for selected website. 
+#'
+#' It loads the most recent workspace saved for a given website based on the date included in the filename of the .Rdata file. 
+#' @param nameOfProject Name of 'castarter' project. Must correspond to the name of a folder in the current working directory. 
+#' @param nameOfWebsite Name of a website included in a 'castarter' project. Must correspond to the name of a sub-folder of the project folder.
+#' @export
+#' @examples
+#' LoadLatestWebsite(nameOfProject, nameOfWebsite)
 LoadLatestWebsite <- function(nameOfProject, nameOfWebsite) {
     lastSavedFile <- file.path(file.path(nameOfProject, nameOfWebsite), sort(list.files(file.path(nameOfProject, nameOfWebsite))[str_extract(list.files(file.path(nameOfProject, 
         nameOfWebsite)), "RData") == "RData"], decreasing = TRUE)[1])
@@ -48,6 +63,15 @@ LoadLatestWebsite <- function(nameOfProject, nameOfWebsite) {
     }
 }
 
+#' Saves working environment and dataset.
+#'
+#' It saves the working environment in the website folder, the dataset as an .Rdata file in the dataset subfolder, and, if exportXlsx = TRUE (by default), exports it as an .xlsx file in the Dataset sub-folder.
+#' @param nameOfProject Name of 'castarter' project. Must correspond to the name of a folder in the current working directory. 
+#' @param nameOfWebsite Name of a website included in a 'castarter' project. Must correspond to the name of a sub-folder of the project folder.
+#' @param exportXlsx If equal to TRUE, exports the complete dataset in the .xlsx file format in the Dataset sub-folder.
+#' @export
+#' @examples
+#' LoadLatestWebsite(nameOfProject, nameOfWebsite)
 SaveAndExportWebsite <- function(nameOfProject, nameOfWebsite, exportXlsx = TRUE) {
     ## Save environment
     save.image(file = file.path(nameOfProject, nameOfWebsite, paste0(paste(Sys.Date(), nameOfProject, nameOfWebsite, sep = " - "), ".RData")))
