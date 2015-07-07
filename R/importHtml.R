ec912d6bf22e6c25a0173e3f78b3d3ecdac32702
Giorgio <giorgiocomai@gmail.com>
2015-07-07 12:31:11 +0200

    Added options to ImportHtml to import from local folders and saving filepath in a data.frame along with html contents.

--- R/importHtml.R
+++ R/importHtml.R
@@ -9,21 +9,30 @@
 #' @export
 #' @examples
 #' articlesHtml <- ImportHtml(from = articles, nameOfProject, nameOfWebsite)
-ImportHtml <- function(from = "", nameOfProject = "", nameOfWebsite = "", pathToHtmlFolder = "") {
+ImportHtml <- function(from = "", nameOfProject = "", nameOfWebsite = "", pathToHtmlFolder = "", sort = TRUE, recursive = TRUE, includePath = FALSE) {
     if (from == "articles") {
         htmlFilesList <- list.files(file.path(nameOfProject, nameOfWebsite, "Html"), pattern = "\\.html$", full.names = TRUE)
     } else if (from == "index") {
         htmlFilesList <- list.files(file.path(nameOfProject, nameOfWebsite, "IndexHtml"), pattern = "\\.html$", full.names = TRUE)
     } else {
+        if (recursive = TRUE) {
+            htmlFilesList <- list.files(pathToHtmlFolder, pattern = "\\.html$", full.names = TRUE, recursive = TRUE)
+        } else {
         htmlFilesList <- list.files(pathToHtmlFolder, pattern = "\\.html$", full.names = TRUE)
+        }
     }
-    htmlFilesList <- mixedsort(htmlFilesList)
+    if (sort == TRUE) {
+        htmlFilesList <- mixedsort(htmlFilesList)
+    }
     numberOfArticles <- length(htmlFilesList)
     htmlFiles <- rep(NA, numberOfArticles)
     for (i in 1:numberOfArticles) {
         htmlFile <- readLines(htmlFilesList[i])
         htmlFile <- paste(htmlFile, collapse = "\n")
         htmlFiles[i] <- htmlFile
+    }
+    if (includePath == TRUE) {
+        htmlFiles <- cbind(htmlFiles, htmlFilesList)
     }
     htmlFiles
 }
