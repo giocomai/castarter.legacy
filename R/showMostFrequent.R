82502e3594d53c258145896484e30b68bbc7f0da
Giorgio Comai <giorgiocomai@gmail.com>
2015-10-09 21:10:28 +0100

    explicit packages

--- R/showMostFrequent.R
+++ R/showMostFrequent.R
@@ -17,13 +17,13 @@
 #' mostFrequent <- ShowMostFrequent(corpusDtm)
 
 ShowMostFrequent <- function(corpusDtm, mode = "vector", number = 10, stemCompletion = FALSE, corpusOriginal = "", minFrequency = 0) {
-    freq <- sort(col_sums(corpusDtm, na.rm = TRUE), decreasing = TRUE)
+    freq <- sort(slam::col_sums(corpusDtm, na.rm = TRUE), decreasing = TRUE)
     if (number == "all") {
         number <- length(freq)
     }
     wordFrequency <- data.frame(word = names(freq), freq = freq)[1:number, ]
     if (stemCompletion == TRUE) {
-        wordFrequency$word <- stemCompletion(wordFrequency$word, corpusOriginal)
+        wordFrequency$word <- tm::stemCompletion(wordFrequency$word, corpusOriginal)
         for (i in 1:length(wordFrequency$word)) {
             if (wordFrequency$word[i] == "") {
                 wordFrequency$word[i] <- row.names(wordFrequency)[i]
@@ -31,9 +31,9 @@ ShowMostFrequent <- function(corpusDtm, mode = "vector", number = 10, stemComple
         }
     }
     if (mode == "barchart" | mode == "graph") {
-        ggplot(data = wordFrequency, aes(x = reorder(word, -freq), y = freq)) + geom_bar(stat = "identity") + coord_flip()
+        ggplot2::ggplot(data = wordFrequency, aes(x = reorder(word, -freq), y = freq)) + geom_bar(stat = "identity") + coord_flip()
     } else if (mode == "wordcloud") {
-        wordcloud(rownames(wordFrequency), wordFrequency$freq, min.freq = minFrequency, colors = "black")
+        wordcloud::wordcloud(rownames(wordFrequency), wordFrequency$freq, min.freq = minFrequency, colors = "black")
     } else if (mode == "vector") {
         wordFrequency
     }
