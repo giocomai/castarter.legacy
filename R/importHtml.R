4dcf4931f919e550fd824a5c27ff7fa372921e37
Giorgio Comai <giorgiocomai@gmail.com>
2015-10-21 10:25:07 +0100

    explicit gtools

--- R/importHtml.R
+++ R/importHtml.R
@@ -23,7 +23,7 @@ ImportHtml <- function(from = "", nameOfProject = "", nameOfWebsite = "", pathTo
         }
     }
     if (sort == TRUE) {
-        htmlFilesList <- mixedsort(htmlFilesList)
+        htmlFilesList <- gtools::mixedsort(htmlFilesList)
     }
     numberOfArticles <- length(htmlFilesList)
     htmlFiles <- rep(NA, numberOfArticles)
@@ -40,7 +40,7 @@ ImportHtml <- function(from = "", nameOfProject = "", nameOfWebsite = "", pathTo
 
 RenumberArticles <- function(nameOfProject = "", nameOfWebsite = "") {
     htmlFilesList <- list.files(file.path(nameOfProject, nameOfWebsite, "Html"), pattern = "\\.html$", full.names = TRUE)
-    htmlFilesList <- mixedsort(htmlFilesList)
+    htmlFilesList <- gtools::mixedsort(htmlFilesList)
     numberOfArticles <- length(htmlFilesList)
     htmlFilesListNew <- file.path(nameOfProject, nameOfWebsite, "Html", paste0(1:numberOfArticles, ".html"))
     file.rename(htmlFilesList, htmlFilesListNew)
