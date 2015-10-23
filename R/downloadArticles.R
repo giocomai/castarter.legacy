33142135f4dd40e5ed943a4574deb0ab2819232d
Giorgio Comai <giorgiocomai@gmail.com>
2015-10-23 17:11:36 +0100

    small fix notification of update

--- R/downloadArticles.R
+++ R/downloadArticles.R
@@ -71,7 +71,7 @@ ReDownloadMissingArticles <- function(nameOfProject, nameOfWebsite, links = arti
         for (i in articlesLinks[linksToDownload]) {
             articleId <- articlesId[linksToDownload][temp]
             system(paste("wget", sQuote(i), "-O", file.path(nameOfProject, nameOfWebsite, "Html", paste0(articleId, ".html"))))
-            print(paste("Downloaded article", temp, "of", length(articlesLinks[linksToDownload]), ". ArticleID: ", articleId, quote = FALSE))
+            print(paste("Downloaded article", temp, "of", length(articlesLinks[linksToDownload]), ". ArticleID: ", articleId), quote = FALSE)
             htmlFile <- readLines(file.path(nameOfProject, nameOfWebsite, "Html", paste0(articleId, ".html")))
             htmlFile <- paste(htmlFile, collapse = "\n")
             articlesHtml[linksToDownload][temp] <- htmlFile
@@ -83,7 +83,7 @@ ReDownloadMissingArticles <- function(nameOfProject, nameOfWebsite, links = arti
             articleId <- articlesId[linksToDownload][temp]
             htmlFile <- RCurl::getURL(i, timeout = 20)
             write(htmlFile, file = file.path(nameOfProject, nameOfWebsite, "Html", paste0(articleId, ".html")))
-            print(paste("Downloaded article", temp, "of", length(articlesLinks[linksToDownload]), ". ArticleID: ", articleId, quote = FALSE))
+            print(paste("Downloaded article", temp, "of", length(articlesLinks[linksToDownload]), ". ArticleID: ", articleId), quote = FALSE)
             temp <- temp + 1
             Sys.sleep(wait)
         }
