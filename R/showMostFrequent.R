1c9ab0f87e8d0583c8179b779c08014a52c7c74d
Giorgio Comai <giorgiocomai@gmail.com>
2015-10-23 10:54:02 +0100

    Introduced export for showMostFrequent

--- R/showMostFrequent.R
+++ R/showMostFrequent.R
@@ -10,14 +10,17 @@
 ##'  \item{"wordcloud"}{: Outputs a wordcloud.}
 ##' }
 ##'
+#' @param corpusDtm A document-term matrix. Can be obtained with 'DocumentTermMatrix(corpus)'
 #' @param specificTerms A character vector, defaults to NULL. If specificTerms is provided, only terms included in this vector will be included in the output.
+#' @param export Logical, defaults to FALSE. If TRUE, saves the time series in both png and pdf format. If nameOfProject and nameOfWebsite are provided, in saves the barchart in the "Outputs" subfolder. 
+#' @param filename A character vector, defaults to NULL.It allows to customize the filename of the exported barchart file. 
 #' @return A data.frame, barchart, or wordcloud as defined with the 'mode' parameter.
 #' @export
 #' @examples
 #' corpusDtm <- DocumentTermMatrix(corpus).
 #' mostFrequent <- ShowMostFrequent(corpusDtm)
 
-ShowMostFrequent <- function(corpusDtm, mode = "data.frame", number = 10, specificTerms = NULL, stemCompletion = FALSE, corpusOriginal = "", minFrequency = 0) {
+ShowMostFrequent <- function(corpusDtm, mode = "data.frame", number = 10, specificTerms = NULL, stemCompletion = FALSE, corpusOriginal = "", minFrequency = 0, export = FALSE, filename = NULL) {
     freq <- sort(slam::col_sums(corpusDtm, na.rm = TRUE), decreasing = TRUE)
     if (is.null(specificTerms) == FALSE) {
         freq <- freq[base::match(specificTerms, names(freq))]
@@ -38,6 +41,33 @@ ShowMostFrequent <- function(corpusDtm, mode = "data.frame", number = 10, specif
     }
     if (mode == "barchart" | mode == "graph") {
         ggplot2::ggplot(data = wordFrequency, ggplot2::aes(x = reorder(term, -freq), y = freq)) + ggplot2::geom_bar(stat = "identity") + ggplot2::coord_flip() + ggplot2::ylab("Word frequency") + ggplot2::xlab("")
+        if (export == TRUE) {
+            if (is.null(filename) == TRUE) {
+                filename <- "word frequency barchart"
+            }
+                if (is.null(nameOfProject) == FALSE & is.null(nameOfWebsite) == FALSE) {
+                if (file.exists(file.path(nameOfProject, nameOfWebsite, "Outputs")) == FALSE) {
+                    dir.create(file.path(nameOfProject, nameOfWebsite, "Outputs"))
+                }
+                ggplot2::ggsave(file.path(nameOfProject, nameOfWebsite, "Outputs", paste0(paste(filename, nameOfProject, nameOfWebsite, sep = " - "), ".png")))
+                print(paste("File saved in", file.path(nameOfProject, nameOfWebsite, "Outputs", paste0(paste(filename, nameOfProject, nameOfWebsite, sep = " - "), ".png"))))
+                ggplot2::ggsave(file.path(nameOfProject, nameOfWebsite, "Outputs", paste0(paste(filename, nameOfProject, nameOfWebsite, sep = " - "), ".pdf")))
+                print(paste("File saved in", file.path(nameOfProject, nameOfWebsite, "Outputs", paste0(paste(filename, nameOfProject, nameOfWebsite, sep = " - "), ".pdf"))))
+            } else if (is.null(nameOfProject) == FALSE & is.null(nameOfWebsite) == TRUE) {
+                ggplot2::ggsave(file.path("Outputs", paste0(paste(filename, nameOfProject, sep = " - "), ".png")))
+                print(paste("File saved in", file.path("Outputs", paste0(paste(filename, nameOfProject, sep = " - "), ".png"))))
+                ggplot2::ggsave(file.path("Outputs", paste0(paste(filename, nameOfProject, sep = " - "), ".pdf")))
+                print(paste("File saved in", file.path("Outputs", paste0(paste(filename, nameOfProject, sep = " - "), ".pdf"))))
+            } else {
+                if (!file.exists(file.path("Outputs"))) {
+                    dir.create(file.path("Outputs"))
+                }
+                ggplot2::ggsave(file.path("Outputs", paste0(paste(filename, sep = " - "), ".png")))
+                print(paste("File saved in", file.path("Outputs", paste0(paste(filename, sep = " - "), ".png"))))
+                ggplot2::ggsave(file.path("Outputs", paste0(paste(filename, sep = " - "), ".pdf")))
+                print(paste("File saved in", file.path("Outputs", paste0(paste(filename, sep = " - "), ".pdf"))))
+            }
+        }
     } else if (mode == "wordcloud") {
         wordcloud::wordcloud(rownames(wordFrequency), wordFrequency$freq, min.freq = minFrequency, colors = "black")
     } else if (mode == "data.frame") {
