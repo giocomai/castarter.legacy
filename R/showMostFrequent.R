f1c621716f8be5aee9d2748a9e35b9df2182cc4d
Giorgio Comai <giorgiocomai@gmail.com>
2015-10-23 11:23:39 +0100

    Added option to define title

--- R/showMostFrequent.R
+++ R/showMostFrequent.R
@@ -13,14 +13,14 @@
 #' @param corpusDtm A document-term matrix. Can be obtained with 'DocumentTermMatrix(corpus)'
 #' @param specificTerms A character vector, defaults to NULL. If specificTerms is provided, only terms included in this vector will be included in the output.
 #' @param export Logical, defaults to FALSE. If TRUE, saves the time series in both png and pdf format. If nameOfProject and nameOfWebsite are provided, in saves the barchart in the "Outputs" subfolder. 
-#' @param filename A character vector, defaults to NULL.It allows to customize the filename of the exported barchart file. 
+#' @param title A character vector, defaults to NULL.It allows to customize the title of the exported barchart file. 
 #' @return A data.frame, barchart, or wordcloud as defined with the 'mode' parameter.
 #' @export
 #' @examples
 #' corpusDtm <- DocumentTermMatrix(corpus).
 #' mostFrequent <- ShowMostFrequent(corpusDtm)
 
-ShowMostFrequent <- function(corpusDtm, mode = "data.frame", number = 10, specificTerms = NULL, stemCompletion = FALSE, corpusOriginal = "", minFrequency = 0, export = FALSE, filename = NULL) {
+ShowMostFrequent <- function(corpusDtm, mode = "data.frame", number = 10, specificTerms = NULL, stemCompletion = FALSE, corpusOriginal = "", minFrequency = 0, export = FALSE, title = NULL, nameOfProject, nameOfWebsite) {
     freq <- sort(slam::col_sums(corpusDtm, na.rm = TRUE), decreasing = TRUE)
     if (is.null(specificTerms) == FALSE) {
         freq <- freq[base::match(specificTerms, names(freq))]
@@ -42,32 +42,32 @@ ShowMostFrequent <- function(corpusDtm, mode = "data.frame", number = 10, specif
     }
     if (mode == "barchart" | mode == "graph") {
         barchart <- ggplot2::ggplot(data = wordFrequency, ggplot2::aes(x = reorder(term, -freq), y = freq)) + ggplot2::geom_bar(stat = "identity") + ggplot2::coord_flip() + ggplot2::ylab("Word frequency") + ggplot2::xlab("")
-        barchart
+        barchart + ggplot2::ggtitle(title)
         if (export == TRUE) {
-            if (is.null(filename) == TRUE) {
-                filename <- "word frequency barchart"
+            if (is.null(title) == TRUE) {
+                title <- "Word frequency barchart"
             }
                 if (is.null(nameOfProject) == FALSE & is.null(nameOfWebsite) == FALSE) {
                 if (file.exists(file.path(nameOfProject, nameOfWebsite, "Outputs")) == FALSE) {
                     dir.create(file.path(nameOfProject, nameOfWebsite, "Outputs"))
                 }
-                ggplot2::ggsave(file.path(nameOfProject, nameOfWebsite, "Outputs", paste0(paste(filename, nameOfProject, nameOfWebsite, sep = " - "), ".png")))
-                print(paste("File saved in", file.path(nameOfProject, nameOfWebsite, "Outputs", paste0(paste(filename, nameOfProject, nameOfWebsite, sep = " - "), ".png"))))
-                ggplot2::ggsave(file.path(nameOfProject, nameOfWebsite, "Outputs", paste0(paste(filename, nameOfProject, nameOfWebsite, sep = " - "), ".pdf")))
-                print(paste("File saved in", file.path(nameOfProject, nameOfWebsite, "Outputs", paste0(paste(filename, nameOfProject, nameOfWebsite, sep = " - "), ".pdf"))))
+                ggplot2::ggsave(file.path(nameOfProject, nameOfWebsite, "Outputs", paste0(paste(title, nameOfProject, nameOfWebsite, sep = " - "), ".png")))
+                print(paste("File saved in", file.path(nameOfProject, nameOfWebsite, "Outputs", paste0(paste(title, nameOfProject, nameOfWebsite, sep = " - "), ".png"))))
+                ggplot2::ggsave(file.path(nameOfProject, nameOfWebsite, "Outputs", paste0(paste(title, nameOfProject, nameOfWebsite, sep = " - "), ".pdf")))
+                print(paste("File saved in", file.path(nameOfProject, nameOfWebsite, "Outputs", paste0(paste(title, nameOfProject, nameOfWebsite, sep = " - "), ".pdf"))))
             } else if (is.null(nameOfProject) == FALSE & is.null(nameOfWebsite) == TRUE) {
-                ggplot2::ggsave(file.path("Outputs", paste0(paste(filename, nameOfProject, sep = " - "), ".png")))
-                print(paste("File saved in", file.path("Outputs", paste0(paste(filename, nameOfProject, sep = " - "), ".png"))))
-                ggplot2::ggsave(file.path("Outputs", paste0(paste(filename, nameOfProject, sep = " - "), ".pdf")))
-                print(paste("File saved in", file.path("Outputs", paste0(paste(filename, nameOfProject, sep = " - "), ".pdf"))))
+                ggplot2::ggsave(file.path("Outputs", paste0(paste(title, nameOfProject, sep = " - "), ".png")))
+                print(paste("File saved in", file.path("Outputs", paste0(paste(title, nameOfProject, sep = " - "), ".png"))))
+                ggplot2::ggsave(file.path("Outputs", paste0(paste(title, nameOfProject, sep = " - "), ".pdf")))
+                print(paste("File saved in", file.path("Outputs", paste0(paste(title, nameOfProject, sep = " - "), ".pdf"))))
             } else {
                 if (!file.exists(file.path("Outputs"))) {
                     dir.create(file.path("Outputs"))
                 }
-                ggplot2::ggsave(file.path("Outputs", paste0(paste(filename, sep = " - "), ".png")))
-                print(paste("File saved in", file.path("Outputs", paste0(paste(filename, sep = " - "), ".png"))))
-                ggplot2::ggsave(file.path("Outputs", paste0(paste(filename, sep = " - "), ".pdf")))
-                print(paste("File saved in", file.path("Outputs", paste0(paste(filename, sep = " - "), ".pdf"))))
+                ggplot2::ggsave(file.path("Outputs", paste0(paste(title, sep = " - "), ".png")))
+                print(paste("File saved in", file.path("Outputs", paste0(paste(title, sep = " - "), ".png"))))
+                ggplot2::ggsave(file.path("Outputs", paste0(paste(title, sep = " - "), ".pdf")))
+                print(paste("File saved in", file.path("Outputs", paste0(paste(title, sep = " - "), ".pdf"))))
             }
         }
         barchart
