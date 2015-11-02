5cb0739ecffb65c6d961a7f0a2d87379654348fa
Giorgio Comai <giorgiocomai@gmail.com>
2015-11-02 13:30:21 +0000

    resize plot title

--- R/timeSeries.R
+++ R/timeSeries.R
@@ -41,7 +41,7 @@ CreateTimeSeries <- function(corpus, terms, specificWebsites = NULL, startDate =
     timeSeries <- zoo::autoplot.zoo(termSeries, facets = NULL) +
         ggplot2::ggtitle(paste("Time series of references to", paste(dQuote(terms), collapse = ", "))) +
         ggplot2::scale_x_datetime("Date") +
-        ggplot2::theme(plot.title = ggplot2::element_text(size = ggplot2::rel(1.3)),
+        ggplot2::theme(plot.title = ggplot2::element_text(size = ggplot2::rel(1.2)),
               legend.title = ggplot2::element_text(size = ggplot2::rel(1.2)),
               legend.text = ggplot2::element_text(size = ggplot2::rel(1))) +
         ggplot2::scale_colour_brewer(type = "qual", palette = 6) +
