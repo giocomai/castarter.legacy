71e0b6da3dd05d706015f4795af8cc9389b5d303
Giorgio Comai <giorgiocomai@gmail.com>
2015-11-02 13:28:34 +0000

    thickened line in time series

--- R/timeSeries.R
+++ R/timeSeries.R
@@ -41,11 +41,12 @@ CreateTimeSeries <- function(corpus, terms, specificWebsites = NULL, startDate =
     timeSeries <- zoo::autoplot.zoo(termSeries, facets = NULL) +
         ggplot2::ggtitle(paste("Time series of references to", paste(dQuote(terms), collapse = ", "))) +
         ggplot2::scale_x_datetime("Date") +
-        ggplot2::theme(plot.title = ggplot2::element_text(size = ggplot2::rel(1.5)),
+        ggplot2::theme(plot.title = ggplot2::element_text(size = ggplot2::rel(1.3)),
               legend.title = ggplot2::element_text(size = ggplot2::rel(1.2)),
               legend.text = ggplot2::element_text(size = ggplot2::rel(1))) +
         ggplot2::scale_colour_brewer(type = "qual", palette = 6) +
-        ggplot2::labs(color = "Terms")
+        ggplot2::labs(color = "Terms") +
+        ggplot2::geom_line(size = 1.2)
     if (is.null(nameOfWebsite) == FALSE) {
         timeSeries <- timeSeries + ggplot2::ggtitle(paste("Time series of references to", paste(dQuote(terms), collapse = ", "), "in", nameOfWebsite))
         }
