9753e07a3fc8921bb376aeb0d1e41c7c45a92126
Giorgio Comai <giorgiocomai@gmail.com>
2015-11-04 11:01:16 +0000

    Introducing UpdateDataset (still not functional)

---
+++ R/updateDataset.R
@@ -0,0 +1,19 @@
+#' Updates a dataset
+#' 
+#' Updates a dataset given a previously created parameters export file.
+#'  
+#' @param nameOfProject Name of 'castarter' project. Must correspond to the name of a folder in the current working directory. 
+#' @param nameOfWebsite Name of a website included in a 'castarter' project. Must correspond to the name of a sub-folder of the project folder.
+#' @return A castarter dataset.
+#' @export
+#' @examples
+#' dataset <- UpdateDataset(dataset, nameOfProject, nameOfWebsite)
+UpdateDataset <- function(dataset, nameOfProject, nameOfWebsite, numberOfIndexPages = 10, wget = FALSE, wait = 3) {
+    params <- read.table(file = file.path(nameOfProject, nameOfWebsite, paste(nameOfWebsite, "updateParameters.csv", sep = "-")), stringsAsFactors = FALSE)
+    CreateFolderStructure(nameOfProject = nameOfProject, nameOfWebsite = nameOfWebsite)
+    indexLinks <- CreateLinks(linkFirstChunk = params$param[params$args=="linkFirstChunk"], linkSecondChunk = params$param[params$args=="linkSecondChunk"], startPage = params$param[params$args=="startPage"], endPage = sum(as.integer(params$param[params$args=="startPage"]), numberOfIndexPages), increaseBy = params$param[params$args=="increaseBy"], sortIndexLinks  = params$param[params$args=="sortIndexLinks"])
+    indexHtml <- DownloadIndex(nameOfProject = nameOfProject, nameOfWebsite = nameOfWebsite, indexLinks = indexLinks, wget = wget, wait = wait)
+    ExtractLinks(domain = params$param[params$args=="domain"])
+    
+}
+                
