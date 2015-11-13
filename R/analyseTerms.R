0195b60c618b1bfcdee6355f9f8f16e9068f3468
Giorgio Comai <giorgiocomai@gmail.com>
2015-11-13 21:02:01 +0000

    ReIntroduced functioning AnalyseTerms after the mess

--- R/analyseTerms.R
+++ R/analyseTerms.R
@@ -2,41 +2,32 @@
 #' 
 #' @param corpus A TM corpus. 
 #' @param nameOfProject Name of 'castarter' project. Must correspond to the name of a folder in the current working directory.
-#' @param terms Character vector of terms to be compared. 
+#' @param specificTerms Character vector of terms to be compared. 
 #' @export
 #' @examples
-#' AnalyseTerms(corpus, nameOfProject, terms)
+#' AnalyseTerms(corpus, nameOfProject, specificTerms)
 
-AnalyseTerms <- function(corpus, nameOfProject, terms, mode = "graph", includeOnly = NULL, order = "", tipology = "", frequency = "relative") {
-    namesOfWebsites <- as.factor(unlist(NLP::meta(corpus, "author")))
-    if (length(levels(namesOfWebsites)) > 1) {
-        
-    } else {
-        
-    }
-    byWebsiteAll <- castarter::DivideByWebsite(corpus, nameOfProject)
-    if (is.null(includeOnly) == FALSE) {
-        byWebsite <- byWebsiteAll[, includeOnly]
-    } else {
-        for (i in 1:length(levels(namesOfWebsites))) {
-            
-        }
-        byWebsite <- byWebsiteAll
-    }
-    mostFrequentByWebsite <- as.data.frame(matrix(nrow = length(byWebsite), ncol = length(terms) + 1))
-    colnames(mostFrequentByWebsite)[1] <- "nameOfWebsite"
-    for (i in 1:length(terms)) {
-        colnames(mostFrequentByWebsite)[i + 1] <- terms[i]
-    }
-    for (i in 1:length(byWebsite)) {
-        corpusTemp <- corpus[byWebsite[, i]]
-        corpusTemp <- tm::tm_map(corpusTemp, PlainTextDocument)
-        dtmTemp <- tm::DocumentTermMatrix(corpusTemp)
-        mostFrequent <- castarter::ShowMostFrequent(dtmTemp, mode = "vector", number = "all")
+AnalyseTerms <- function(corpus, nameOfProject, specificTerms, mode = "graph", includeOnly = NULL, order = "", tipology = "", frequency = "relative") {
+     namesOfWebsites <- levels(as.factor(unlist(NLP::meta(corpus, "author"))))
+     corpusDtm <- CreateDtm(corpus)
+     byWebsiteAll <- DivideByWebsite(corpus = corpus, nameOfProject = nameOfProject)
+     if (is.null(includeOnly) == FALSE) {
+          byWebsite <- byWebsiteAll[, includeOnly]
+     } else {
+          byWebsite <- byWebsiteAll
+     }
+     mostFrequentByWebsite <- as.data.frame(matrix(nrow = length(byWebsite), ncol = length(specificTerms) + 1))
+     colnames(mostFrequentByWebsite)[1] <- "nameOfWebsite"
+     for (i in 1:length(specificTerms)) {
+          colnames(mostFrequentByWebsite)[i + 1] <- specificTerms[i]
+     }
+     for (i in 1:length(byWebsite)) {
+        dtmTemp <- corpusDtm[byWebsite[, i], ]
+        mostFrequent <- castarter::ShowMostFrequent(dtmTemp, mode = "data.frame", number = "all", specificTerms = specificTerms)
         totalWords <- sum(mostFrequent$freq)
         mostFrequentByWebsite[i, 1] <- names(byWebsite)[i]
-        for (j in 1:length(terms)) {
-            value <- mostFrequent$freq[mostFrequent$word == terms[j]]
+        for (j in 1:length(specificTerms)) {
+            value <- mostFrequent$freq[mostFrequent$term == specificTerms[j]]
             if (length(value) == 0) {
                 value <- 0
             } else {
@@ -66,21 +57,20 @@ AnalyseTerms <- function(corpus, nameOfProject, terms, mode = "graph", includeOn
     }
     if (mode == "graph") {
         if (tipology != "") {
-            mostFrequentByWebsiteLong <- reshape2::melt(mostFrequentByWebsite, id.vars = c("nameOfWebsite", "typeOfWebsite"), measure.vars = terms, 
+            mostFrequentByWebsiteLong <- reshape2::melt(mostFrequentByWebsite, id.vars = c("nameOfWebsite", "typeOfWebsite"), measure.vars = specificTerms, 
                 value.name = "frequency")
         } else {
-            mostFrequentByWebsiteLong <- reshape2::melt(mostFrequentByWebsite, id.vars = "nameOfWebsite", measure.vars = terms, value.name = "frequency")
+            mostFrequentByWebsiteLong <- reshape2::melt(mostFrequentByWebsite, id.vars = "nameOfWebsite", measure.vars = specificTerms, value.name = "frequency")
         }
         names(mostFrequentByWebsiteLong)[names(mostFrequentByWebsiteLong) == "variable"] <- "Term"
         mostFrequentByWebsiteLong$nameOfWebsite <- as.factor(mostFrequentByWebsiteLong$nameOfWebsite)
         # mostFrequentByWebsiteLong$typeOfWebsite <- as.factor(mostFrequentByWebsiteLong$typeOfWebsite)
         # factor(mostFrequentByWebsiteLong$nameOfWebsite, levels = rev(levels(mostFrequentByWebsiteLong$typeOfWebsite)))
-        mentionterms <- paste(dQuote(rev(terms)), collapse = ", ")
-        mostFrequentByWebsite <- ggplot2::ggplot(mostFrequentByWebsiteLong, aes(x = nameOfWebsite, y = frequency, fill = Term)) + geom_bar(stat = "identity", 
-            position = "dodge", colour = "black") + ggtitle(paste("Frequency of", mentionterms)) + scale_x_discrete(name = "") + scale_y_continuous(name = "Frequency of term as % of all words", 
-            labels = percent) + guides(fill = guide_legend(reverse = TRUE)) + coord_flip()
+        mentionterms <- paste(dQuote(rev(specificTerms)), collapse = ", ")
+        mostFrequentByWebsite <- ggplot2::ggplot(mostFrequentByWebsiteLong, ggplot2::aes(x = nameOfWebsite, y = frequency, fill = Term)) + ggplot2::geom_bar(stat = "identity", 
+            position = "dodge") + ggplot2::ggtitle(paste("Frequency of", mentionterms)) + ggplot2::scale_x_discrete(name = "") + ggplot2::scale_y_continuous(name = "Frequency of term as % of all words", labels = percent) + ggplot2::guides(fill = ggplot2::guide_legend(reverse = TRUE)) + ggplot2::coord_flip()
         if (order == "frequency") {
-            mostFrequentByWebsite <- mostFrequentByWebsite + scale_x_discrete(limits = dataframeMostFrequentByWebsite$nameOfWebsite)
+            mostFrequentByWebsite <- mostFrequentByWebsite + ggplot2::scale_x_discrete(limits = dataframeMostFrequentByWebsite$nameOfWebsite)
         }
     }
     mostFrequentByWebsite
