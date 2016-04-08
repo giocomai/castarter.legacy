fd6c5a9a19a56cfbffcd2fe72c471eaed1d1b6cb
Giorgio Comai <giorgiocomai@gmail.com>
2016-04-08 14:50:02 +0400

    Introduce working verion of UploadDatasets

--- R/UploadDownload.R
+++ R/UploadDownload.R
@@ -3,14 +3,15 @@
 #' Uploads a set of datasets from existings 'castarter' projects to an online repository.
 #'  
 #' @param projectsAndWebsites A character vector listing websites to be loaded in the format "nameOfProject/nameOfWebsite".
-#' @return A data frame including all loaded datasets.
+#' @return Nothing. Used for its side effects (uploading datasets to an ftp server).
 #' @export
 #' @examples
 #' projectsAndWebsites <- c("ProjectX/Website1", "ProjectY/Website3", "ProjectZ/Website2")
-#' UploadDataset(projectsAndWebsites)
-UploadDatasets <- function(projectsAndWebsites, removeNAdates = TRUE) {
+#' UploadDataset(projectsAndWebsites, server = "myserver.com", user = "me@myserver.com", pwd = "secretPassword!")
+UploadDatasets <- function(projectsAndWebsites, removeNAdates = TRUE, server, user, pwd) {
     projectsAndWebsites <- base::strsplit(projectsAndWebsites, "/")
     lastSavedDatasets <- vector()
+    datasetFilenames <- vector()
     for (i in 1:length(projectsAndWebsites)) {
         nameOfProject <- projectsAndWebsites[[i]][1]
         nameOfWebsite <- projectsAndWebsites[[i]][2]
@@ -18,21 +19,20 @@ UploadDatasets <- function(projectsAndWebsites, removeNAdates = TRUE) {
         if (is.na(datasetFilename) == FALSE) {
             lastSavedDataset <- file.path(file.path(nameOfProject, nameOfWebsite, "Dataset"), datasetFilename)
             lastSavedDatasets[i] <- lastSavedDataset
+            datasetFilenames[i] <- datasetFilename
         }
         lastSavedDatasets <- lastSavedDatasets[!is.na(lastSavedDatasets)]
     }
+    opts <- list(ftp.create.missing.dirs=TRUE)
     for (i in 1:length(lastSavedDatasets)) {
-        RCurl::ftpUpload(what = lastSavedDatasets[1], to = "", userpwd = "")
-        
+        RCurl::ftpUpload(what = lastSavedDatasets[i],
+                         to = paste0("ftp://", server, ":21/",
+                                     paste(projectsAndWebsites[[i]][1], projectsAndWebsites[[i]][2], sep = "/"),
+                                     "/", datasetFilenames[i]),
+                         userpwd = paste(user, pwd, sep = ":"),
+                         .opts=opts)
     }
-    allDatasets <- data.frame()
-    for (i in 1:length(lastSavedDatasets)) {
-        load(lastSavedDatasets[i])
-        allDatasets <- rbind(allDatasets, dataset)
-        rm(dataset)
-    }
-    if (removeNAdates == TRUE) {
-        allDatasets <- allDatasets[is.na(allDatasets$dates) == FALSE, ]
-    }
-    allDatasets
+    # if (removeNAdates == TRUE) {
+    #     allDatasets <- allDatasets[is.na(allDatasets$dates) == FALSE, ]
+    # }
 }
