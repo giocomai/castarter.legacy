2698f730857ef1638c31d87a3dce1f52e1133d67
Giorgio Comai <giorgiocomai@gmail.com>
2016-04-12 15:45:46 +0400

    introduced option to provide dataset, but left temporary solution for metadata

--- R/preliminary.R
+++ R/preliminary.R
@@ -50,22 +50,24 @@ LoadLatest <- function(nameOfProject, nameOfWebsite) {
 #' It assumes that previous operations have been completed, and both a 'metadata' and an 'articlesTxt' object exist. If they don't, it just prints this to the console. 
 #' @param nameOfProject Name of 'castarter' project. Must correspond to the name of a folder in the current working directory. 
 #' @param nameOfWebsite Name of a website included in a 'castarter' project. Must correspond to the name of a sub-folder of the project folder.
+#' @param dataset A 'castarter' dataset. Required to export dataset in its dedicated folder if no metadata data.frame is provided, e.g. because dataset has been created through CreateDatasetFromHtml() function.
 #' @param exportXlsx If equal to TRUE, exports the complete dataset in the .xlsx file format in the Dataset sub-folder.
+#' @return Nothing. Used for its side effects (save current workspace and dataset in relevant folders).
 #' @export
 #' @examples
-#' LoadLatestWebsite(nameOfProject, nameOfWebsite)
-SaveAndExportWebsite <- function(nameOfProject, nameOfWebsite, exportXlsx = FALSE) {
+#' SaveAndExportWebsite(nameOfProject, nameOfWebsite)
+SaveAndExportWebsite <- function(nameOfProject, nameOfWebsite, dataset = NULL, exportXlsx = FALSE) {
     ## Save environment
     save.image(file = file.path(nameOfProject, nameOfWebsite, paste0(paste(Sys.Date(), nameOfProject, nameOfWebsite, sep = " - "), ".RData")))
     print(paste("Environment saved in", file.path(nameOfProject, nameOfWebsite, paste0(paste(Sys.Date(), nameOfProject, nameOfWebsite, sep = " - "), ".RData"))))
-    ## Save dataset
-    if (exists("metadata")==FALSE) {
-        print("Metadata not available, the dataset has not been saved separately.")
-    } else {
-    dataset <- cbind(metadata, articlesTxt, stringsAsFactors = FALSE)
+    if (gtools::invalid(dataset) == TRUE) {
+        if (exists("metadata") == FALSE) {
+            print("Metadata not available, the dataset has not been saved separately.")
+        } 
+        dataset <- cbind(metadata, articlesTxt, stringsAsFactors = FALSE)
+    }
     save(dataset, file = file.path(nameOfProject, nameOfWebsite, "Dataset", paste0(paste(Sys.Date(), nameOfProject, nameOfWebsite, "dataset", sep = " - "), ".RData")))
     print(paste("Dataset saved in", file.path(nameOfProject, nameOfWebsite, paste0(paste(Sys.Date(), nameOfProject, nameOfWebsite, "dataset", sep = " - "), ".RData"))))
-    }
     # export all as xlsx
     if (exportXlsx == TRUE) {
         xlsx::write.xlsx(cbind(metadata, articlesTxt), file.path(nameOfProject, nameOfWebsite, "Dataset", paste0(paste(Sys.Date(), nameOfProject, nameOfWebsite, 
