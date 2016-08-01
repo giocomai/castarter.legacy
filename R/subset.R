d5ca5e5644674ed32a0324f406d42cc1adb9dcdd
Giorgio Comai <giorgiocomai@gmail.com>
2016-08-01 14:03:27 +0200

    remove unimplemented functions

--- R/subset.R
+++ R/subset.R
@@ -41,17 +41,3 @@ DivideByWebsite <- function(corpus, nameOfProject) {
     }
     byWebsite
 }
-
-# SubsetByDate <- function(corpus) { listOfDates <- seq(start(docSeries), end(docSeries), 'DSTday') byDate <- data.frame(matrix(NA, nrow =
-# length(corpus), ncol = length(listOfDates))) names(byDate) <- listOfDates for (i in 1:length(listOfDates)) { byDate[, i] <- meta(corpus,
-# 'datetimestamp') == listOfDates[i] } byDate }
-
-IncludeOnly <- function(corpus, keepOnly, removeCharacterOrWord = "") {
-    Include <- content_transformer(function(x, pattern) regmatches(x, gregexpr(pattern, x, perl = TRUE)))
-    keepOnly <- gsub("[[:punct:]]+", "", keepOnly)
-    keepOnly <- gsub("[[:space:]]+", "", keepOnly)
-    keepOnly <- paste(keepOnly, collapse = "|")
-    corpus <- tm_map(corpus, Include, keepOnly)
-    corpus <- CleanCorpus(corpus, removeCharacter = removeCharacterOrWord)
-    corpus
-} 
