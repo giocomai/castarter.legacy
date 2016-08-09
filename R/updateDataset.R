ac7bfdd0f435d262ea9db71b56a9651e7cfa168a
Giorgio Comai <giorgiocomai@gmail.com>
2016-08-09 21:57:12 +0200

    multiple updates to function and params names

--- R/updateDataset.R
+++ R/updateDataset.R
@@ -8,7 +8,7 @@
 #' @export
 #' @examples
 #' dataset <- UpdateDataset(dataset)
-UpdateDataset <- function(dataset, numberOfIndexPages = 10, wget = FALSE, wait = 3, nameOfProject = NULL, nameOfWebsite = NULL) {
+UpdateDataset <- function(dataset, articlesLinksNew = NULL, numberOfIndexPages = 10, wget = FALSE, wait = 3, nameOfProject = NULL, nameOfWebsite = NULL) {
     if (gtools::invalid(nameOfProject) == TRUE) {
         nameOfProject <- CastarterOptions("nameOfProject")
     }
@@ -18,28 +18,25 @@ UpdateDataset <- function(dataset, numberOfIndexPages = 10, wget = FALSE, wait =
     params <- read.table(file = file.path(nameOfProject, nameOfWebsite, paste(nameOfWebsite, "Logs", "updateParameters.csv", sep = "-")), stringsAsFactors = FALSE)
     params$param[params$param == "NULL"] <- NA
     params$param[params$param == ""] <- NA
-    CreateFolderStructure(nameOfProject = nameOfProject, nameOfWebsite = nameOfWebsite)
     castarter::CreateFolders(nameOfProject = nameOfProject, nameOfWebsite = nameOfWebsite)
-    indexLinks <- CreateLinks(linkFirstChunk = params$param[params$args=="linkFirstChunk"], linkSecondChunk = params$param[params$args=="linkSecondChunk"], startPage = as.integer(params$param[params$args=="startPage"]), endPage = sum(as.integer(params$param[params$args=="startPage"]), numberOfIndexPages), increaseBy = as.integer(params$param[params$args=="increaseBy"]), sortIndexLinks  = as.logical(params$param[params$args=="sortIndexLinks"]))
-    indexHtml <- DownloadIndex(nameOfProject = nameOfProject, nameOfWebsite = nameOfWebsite, indexLinks = indexLinks, wget = wget, wait = wait)
-    articlesLinksNew <- ExtractLinks(domain = params$param[params$args=="domain"], partOfLink = params$param[params$args=="partOfLink"], indexHtml = indexHtml, containerType = params$param[params$args=="containerTypeExtractLinks"], containerClass = params$param[params$args=="containerClassExtractLinks"], divClass = params$param[params$args=="divClassExtractLinks"], partOfLinkToExclude = strsplit(params$param[params$args=="partOfLinkToExclude"], "§§§"))
-    articlesLinksNew <- articlesLinksNew[is.element(articlesLinksNew, dataset$articlesLinks)==FALSE]
+    if (base::is.null(articlesLinksNew)==TRUE) {
+        indexLinks <- CreateLinks(linkFirstChunk = params$param[params$args=="linkFirstChunk"], linkSecondChunk = params$param[params$args=="linkSecondChunk"], startPage = as.integer(params$param[params$args=="startPage"]), endPage = sum(as.integer(params$param[params$args=="startPage"]), numberOfIndexPages), increaseBy = as.integer(params$param[params$args=="increaseBy"]), sortIndexLinks  = as.logical(params$param[params$args=="sortIndexLinks"]))
+        indexHtml <- DownloadIndex(nameOfProject = nameOfProject, nameOfWebsite = nameOfWebsite, indexLinks = indexLinks, wget = wget, wait = wait)
+        articlesLinksNew <- ExtractLinks(domain = params$param[params$args=="domain"], partOfLink = params$param[params$args=="partOfLink"], indexHtml = indexHtml, containerType = params$param[params$args=="containerTypeExtractLinks"], containerClass = params$param[params$args=="containerClassExtractLinks"], divClass = params$param[params$args=="divClassExtractLinks"], partOfLinkToExclude = strsplit(params$param[params$args=="partOfLinkToExclude"], "§§§"))
+        articlesLinksNew <- articlesLinksNew[is.element(articlesLinksNew, dataset$articlesLinks)==FALSE]
+    }
     previousLength <- length(dataset$articlesLinks)
     articlesLinks <- c(dataset$articlesLinks, articlesLinksNew)
     DownloadContents(links = articlesLinks, type = "articles", nameOfProject = nameOfProject, nameOfWebsite = nameOfWebsite, wget = wget, missingArticles = TRUE, wait = wait, start = sum(previousLength, 1))
     DownloadContents(links = articlesLinksNew, type = "articles", nameOfProject = nameOfProject, nameOfWebsite = nameOfWebsite, wget = wget, missingArticles = FALSE, wait = wait)
     articlesHtmlNew <- ImportHtml(from = "articles", nameOfProject = nameOfProject, nameOfWebsite = nameOfWebsite)
     titles <- ExtractTitles(articlesHtml = articlesHtmlNew, articlesLinks = articlesLinksNew, titlesExtractMethod = params$param[params$args=="titlesExtractMethod"], removePunctuation = params$param[params$args=="removePunctuationExtractTitles"], onlyStandardCharacters = params$param[params$args=="onlyStandardCharactersExtractTitles"], removeString = params$param[params$args=="removeStringExtractTitles"], removeEverythingAfter = params$param[params$args=="removeEverythingAfterExtractTitles"], customXpath = params$param[params$args=="customXpathExtractTitles"], maxCharacters = params$param[params$args=="maxCharactersExtractTitles"])
-    if (gtools::invalid(params$param[params$args=="divClassDatesXpath"]) == TRUE & gtools::invalid(params$param[params$args=="spanClassDatesXpath"]) == TRUE & gtools::invalid(params$param[params$args=="customXpathDates"]) == TRUE) {
-        dates <- ExtractDates(articlesHtml = articlesHtmlNew, dateFormat = params$param[params$args=="dateFormat"], language = dataset$language[1], customString = params$param[params$args=="customStringExtractDates"], minDate = params$param[params$args=="minDate"], maxDate = params$param[params$args=="maxDate"], removeEverythingBefore = params$param[params$args=="removeEverythingBeforeExtractDates"])
-    } else {
-        dates <- ExtractDatesXpath(articlesHtml = articlesHtmlNew, dateFormat = params$param[params$args=="dateFormat"], divClass = params$param[params$args=="divClassDatesXpath"], spanClass = params$param[params$args=="spanClassDatesXpath"], customXpath = params$param[params$args=="customXpathDates"], language = dataset$language[1], customString = params$param[params$args=="customStringExtractDates"], minDate = params$param[params$args=="minDate"], maxDate = params$param[params$args=="maxDate"], keepAllString = params$param[params$args=="keepAllString"])
-    }
+    dates <- ExtractDates(articlesHtml = articlesHtmlNew, dateFormat = params$param[params$args=="dateFormat"], language = dataset$language[1], customString = params$param[params$args=="customStringExtractDates"], divClass = params$param[params$args=="divClassExtractDates"], spanClass = params$param[params$args=="spanClassExtractDates"], customXpath = params$param[params$args=="customXpathExtractDates"], language = dataset$language[1], customString = params$param[params$args=="customStringExtractDates"], keepAllString = params$param[params$args=="keepAllString"], minDate = params$param[params$args=="minDate"], maxDate = params$param[params$args=="maxDate"], removeEverythingBefore = params$param[params$args=="removeEverythingBeforeExtractDates"])
     articlesId <- ExtractArticleId(nameOfProject, nameOfWebsite)
     language <- dataset$language[1]
     metadata <- ExportMetadata(nameOfProject = nameOfProject, nameOfWebsite = nameOfWebsite, dates = dates, articlesId = articlesId, titles = titles, language = language, articlesLinks = articlesLinksNew)
-    articlesTxt <- ExtractTxt(articlesHtml = articlesHtml, metadata = metadata, export = params$param[params$args=="exportExtractTxt"], maxTitleCharacters = params$param[params$args=="maxTitleCharacters"], textToBeRemoved = unlist(stringr::str_split(string = params$param[params$args=="textToBeRemovedExtractTxt"], pattern = "§§§")), divClass = params$param[params$args=="divClassExtractTxt"], divID = params$param[params$args=="divIDExtractTxt"], removeEverythingAfter = params$param[params$args=="removeEverythingAfterExtractTxt"], removeEverythingBefore = params$param[params$args=="removeEverythingBeforeExtractTxt"], removePunctuationInFilename = params$param[params$args=="removePunctuationInFilename"], keepEverything = params$param[params$args=="keepEverything"])
+    articlesTxt <- ExtractTxt(articlesHtml = articlesHtml, metadata = metadata, export = params$param[params$args=="exportExtractTxt"], maxTitleCharacters = params$param[params$args=="maxTitleCharacters"], textToBeRemoved = unlist(stringr::str_split(string = params$param[params$args=="textToBeRemovedExtractTxt"], pattern = "§§§")), divClass = params$param[params$args=="divClassExtractTxt"], divID = params$param[params$args=="divIdExtractTxt"], removeEverythingAfter = params$param[params$args=="removeEverythingAfterExtractTxt"], removeEverythingBefore = params$param[params$args=="removeEverythingBeforeExtractTxt"], removePunctuationInFilename = params$param[params$args=="removePunctuationInFilename"], keepEverything = params$param[params$args=="keepEverything"])
     dataset <- rbind(dataset, cbind(metadata, articlesTxt, stringsAsFactors = FALSE))
-    dataset
+    invisible(dataset)
 }
                 
