93df2efcf78fdef63a29efe07a7bc00d7695a1c7
Giorgio Comai <giorgiocomai@gmail.com>
2016-08-10 15:36:23 +0200

    fix filename

--- R/updateDataset.R
+++ R/updateDataset.R
@@ -15,7 +15,7 @@ UpdateDataset <- function(dataset, articlesLinks = NULL, numberOfIndexPages = 10
     if (gtools::invalid(nameOfWebsite) == TRUE) {
         nameOfWebsite <- CastarterOptions("nameOfWebsite")
     }
-    params <- read.table(file = file.path(nameOfProject, nameOfWebsite, "Logs", paste(nameOfWebsite, "updateParameters.csv", sep = "-")), stringsAsFactors = FALSE)
+    params <- read.table(file = file.path(nameOfProject, nameOfWebsite, "Logs", paste(nameOfWebsite, "updateParameters.csv", sep = " - ")), stringsAsFactors = FALSE)
     params$param[params$param == "NULL"] <- NA
     params$param[params$param == ""] <- NA
     castarter::CreateFolders(nameOfProject = nameOfProject, nameOfWebsite = nameOfWebsite)
