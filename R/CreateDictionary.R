5c78626867a6b3c7a07315e4030cf471110479f2
Giorgio Comai <giorgiocomai@gmail.com>
2016-09-17 18:23:29 +0100

    Introducing CreateDictionary()

---
+++ R/CreateDictionary.R
@@ -0,0 +1,23 @@
+#' Create dictionaries
+#' 
+#' Create dictionaries to be used by castarter functions such as ShowFreq or ShowTS 
+#'  
+#' @param terms A character vector in the format: c("Fruit=Apple+Orange", "Animals=cow+sheep+dog")
+#' @return A dictionary of the 'quanteda' type. 
+#' @export
+#' @examples
+#' CreateDictionary(terms = c("Fruit=Apple+Orange", "Animals=cow+sheep+dog"))
+
+CreateDictionary <- function(terms) {
+    termsSplit <- stringi::stri_split_fixed(str = terms, pattern = "=")
+    termNames <- rep(NA, length(termsSplit))
+    termsL <- list()
+    for (i in seq_along(termsSplit)) {
+        termNames[i] <- termsSplit[[i]][1]
+        termsL[[i]] <- unlist(stringi::stri_split_fixed(str = termsSplit[[i]][2], pattern = "+"))
+    }
+    termsL <- setNames(object = termsL, nm = termNames)
+    termsDic <- quanteda::dictionary(x = termsL)
+    return(termsDic)
+}
+
