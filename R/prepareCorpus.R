c71d645206a9494036f256e0a1d4443dde924ff6
Giorgio Comai <giorgiocomai@gmail.com>
2017-01-30 14:24:15 +0100

    fix links/link

--- R/prepareCorpus.R
+++ R/prepareCorpus.R
@@ -146,7 +146,7 @@ LoadAllDatasets <- function(project, removeNAdates = TRUE) {
 CreateCorpus <- function(dataset, quanteda = TRUE) {
     if (quanteda==TRUE) {
         corpus <- quanteda::corpus(dataset$contents, docnames = paste(as.Date(dataset$date), dataset$id, dataset$title, sep = " - "),
-                                   docvars=data.frame(website=dataset$website, date=as.Date(dataset$date), title=dataset$title, links = dataset$link, ID=dataset$id))
+                                   docvars=data.frame(website=dataset$website, date=as.Date(dataset$date), title=dataset$title, link = dataset$link, ID=dataset$id))
     } else {
         corpus <- tm::VCorpus(tm::VectorSource(dataset$contents))
         for (i in 1:length(dataset$contents)) {
