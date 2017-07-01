773c6d4944d9487c915dc8ecea6342a1af2cd2db
Giorgio Comai <giorgiocomai@gmail.com>
2017-07-01 14:51:31 +0200

    introduce structured and reactive UI for CreateLinks

--- inst/shiny/CreateDataset/server.R
+++ inst/shiny/CreateDataset/server.R
@@ -3,6 +3,25 @@ library("castarter")
 
 shinyServer(function(input, output) {
 
+    # CreateLinks UI
+    output$CreateLinks_UI <- renderUI({
+        if(input$CreateLinks_RadioUI == "Numbers"){
+            fluidRow(class = "ReactiveUI",
+                textInput(inputId = "CreateLinks_startPage", label = "First page", value = 1, placeholder = NULL),
+                textInput(inputId = "CreateLinks_endPage", label = "Last page", value = 10, placeholder = NULL),
+                textInput(inputId = "CreateLinks_increaseBy", label = "Increase by", value = 1, placeholder = NULL)
+            )
+        } else if (input$CreateLinks_RadioUI == "Dates") {
+            fluidRow(
+                radioButtons(inputId = "CreateLinks_dateFormat", label = "Date format, if index link includes a date", choices =
+                                 c("year-month-day" = "ymd",
+                                   "year-month" = "ym",
+                                   "year" = "y"), inline = TRUE),
+                dateRangeInput(inputId = "CreateLinks_DateRange", label = "Select date range")
+            )
+        }
+    })
+
 
 
 })
--- inst/shiny/CreateDataset/ui.R
+++ inst/shiny/CreateDataset/ui.R
@@ -5,25 +5,31 @@ shinyUI(fluidPage(
     titlePanel("Create a textual dataset from a website"),
 
     navlistPanel(
-        tabPanel("Create index links"),
-        fluidRow(h2("Create index links")),
-        fluidRow(
-            textInput(inputId = "CreateLinks_linkFirstChunk", label = "First part of the link", value = "", width = NULL, placeholder = "e.g. http://www.example.com/news/"),
-            textInput(inputId = "CreateLinks_linkSecondChunk", label = "Part of the link to append after varying part", value = NULL, width = NULL, placeholder = "e.g. .html"),
-            splitLayout(
-            textInput(inputId = "CreateLinks_startPage", label = "First page", value = 1, width = 80, placeholder = NULL),
-            textInput(inputId = "CreateLinks_endPage", label = "Last page", value = 10, width = 80, placeholder = NULL),
-            textInput(inputId = "CreateLinks_increaseBy", label = "Increase by", value = 1, width = 80, placeholder = NULL)
-            ),
-            radioButtons(inputId = "CreateLinks_dateFormat", label = "Date format, if index link includes a date", choices =
-                             c("year-month-day" = "ymd",
-                               "year-month" = "ym",
-                               "year" = "y"), selected = character(0), inline = TRUE),
-            dateRangeInput(inputId = "CreateLinks_DateRange", label = "Select date range")
-            ),
+        #### End of CreateLinks page ####
+        tabPanel("Create index links",
+                 fluidPage(theme="style.css",
+
+                     column(6,
+                            h3("Introduce parameters"),
+                         textInput(inputId = "CreateLinks_linkFirstChunk", label = "First part of the link", value = "", width = NULL, placeholder = "e.g. http://www.example.com/news/"),
+                         textInput(inputId = "CreateLinks_linkSecondChunk", label = "Part of the link to append after varying part", value = NULL, width = NULL, placeholder = "e.g. .html"),
+                         radioButtons(inputId = "CreateLinks_RadioUI", label = "Links based on numbers or dates?", choices = c("Numbers", "Dates"), inline = TRUE),
+                         ## Reactive CreateLinks UI
+                         uiOutput("CreateLinks_UI")),
+
+                     column(6,
+
+                     fluidRow(
+                         h3("Preview of generated links")
+                     )
+                     )
+                 )
+        ),
+        #### End of CreateLinks page ####
         tabPanel("Component 2"),
         tabPanel("Component 3"),
         tabPanel("Component 4"),
-        tabPanel("Component 5")
+        tabPanel("Component 5"),
+        widths = c(3, 8)
     )
 ))
