245d717fe64b9057612c1f646a7fa501d66ea371
Giorgio Comai <giorgiocomai@gmail.com>
2017-08-13 18:39:43 +0300

    Introduce SetCastarter page

--- inst/shiny/CreateDataset/server.R
+++ inst/shiny/CreateDataset/server.R
@@ -3,13 +3,32 @@ library("castarter")
 
 shinyServer(function(input, output) {
 
+    ##### SetCastarter options ######
+    output$SetCastarter <- renderUI({
+        input$SetCastarter
+
+        shiny::isolate({
+            if (input$project!="" & input$website!="") {
+                SetCastarter(project = input$project, website = input$website)
+        tryCatch(expr = CreateFolders(), warning = function(w) {
+            if (grepl(pattern = "Permission denied", x = w) == TRUE) {
+                print("Folders could not be created: permission denied.")
+            }
+        })
+        if (file.exists(file.path(input$project, input$website))) {
+            HTML(paste0("Name of project (", sQuote(input$project), ") and website (", sQuote(input$project), ") set for this session.", "<br />", "Folder structure under ", sQuote(paste0(input$project, "/", input$website)), " has been created or already exists."))
+        }
+            }
+        })
+    })
+
     # CreateLinks UI
     output$CreateLinks_UI <- renderUI({
         if(input$CreateLinks_RadioUI == "Numbers"){
             fluidRow(class = "ReactiveUI",
-                textInput(inputId = "CreateLinks_startPage", label = "First page", value = 1, placeholder = NULL),
-                textInput(inputId = "CreateLinks_endPage", label = "Last page", value = 10, placeholder = NULL),
-                textInput(inputId = "CreateLinks_increaseBy", label = "Increase by", value = 1, placeholder = NULL)
+                     textInput(inputId = "CreateLinks_startPage", label = "First page", value = 1, placeholder = NULL),
+                     textInput(inputId = "CreateLinks_endPage", label = "Last page", value = 10, placeholder = NULL),
+                     textInput(inputId = "CreateLinks_increaseBy", label = "Increase by", value = 1, placeholder = NULL)
             )
         } else if (input$CreateLinks_RadioUI == "Dates") {
             fluidRow(
@@ -23,5 +42,6 @@ shinyServer(function(input, output) {
     })
 
 
-
 })
+
+
--- inst/shiny/CreateDataset/ui.R
+++ inst/shiny/CreateDataset/ui.R
@@ -5,24 +5,35 @@ shinyUI(fluidPage(
     titlePanel("Create a textual dataset from a website"),
 
     navlistPanel(
-        #### End of CreateLinks page ####
+        #### Beginning of SetCastarter page ####
+        tabPanel("Set up project",
+                 fluidPage(theme="style.css",
+                           textInput(inputId = "project", label = "Name of project", value = "", width = NULL, placeholder = "e.g. News"),
+                           textInput(inputId = "website", label = "Name of website", value = "", width = NULL, placeholder = "e.g. Name of given news website"),
+                           actionButton(inputId = "SetCastarter", label = "Set project and create relevant folder structure", icon = icon("check"), style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
+                           uiOutput(outputId = "SetCastarter")
+                 )
+        ),
+        #### End of SetCastarter page ####
+
+        #### Beginning of CreateLinks page ####
         tabPanel("Create index links",
                  fluidPage(theme="style.css",
 
-                     column(6,
-                            h3("Introduce parameters"),
-                         textInput(inputId = "CreateLinks_linkFirstChunk", label = "First part of the link", value = "", width = NULL, placeholder = "e.g. http://www.example.com/news/"),
-                         textInput(inputId = "CreateLinks_linkSecondChunk", label = "Part of the link to append after varying part", value = NULL, width = NULL, placeholder = "e.g. .html"),
-                         radioButtons(inputId = "CreateLinks_RadioUI", label = "Links based on numbers or dates?", choices = c("Numbers", "Dates"), inline = TRUE),
-                         ## Reactive CreateLinks UI
-                         uiOutput("CreateLinks_UI")),
+                           column(6,
+                                  h3("Introduce parameters"),
+                                  textInput(inputId = "CreateLinks_linkFirstChunk", label = "First part of the link", value = "", width = NULL, placeholder = "e.g. http://www.example.com/news/"),
+                                  textInput(inputId = "CreateLinks_linkSecondChunk", label = "Part of the link to append after varying part", value = NULL, width = NULL, placeholder = "e.g. .html"),
+                                  radioButtons(inputId = "CreateLinks_RadioUI", label = "Links based on numbers or dates?", choices = c("Numbers", "Dates"), inline = TRUE),
+                                  ## Reactive CreateLinks UI
+                                  uiOutput("CreateLinks_UI")),
 
-                     column(6,
+                           column(6,
 
-                     fluidRow(
-                         h3("Preview of generated links")
-                     )
-                     )
+                                  fluidRow(
+                                      h3("Preview of generated links")
+                                  )
+                           )
                  )
         ),
         #### End of CreateLinks page ####
