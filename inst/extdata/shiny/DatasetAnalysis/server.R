933f7ba8f697d1afb00ccaa50a91b75474602ee1
Giorgio Comai <comai@balcanicaucaso.org>
2018-05-04 23:24:56 +0200

    fix highlight with first keyword when one term given

--- inst/extdata/shiny/DatasetAnalysis/server.R
+++ inst/extdata/shiny/DatasetAnalysis/server.R
@@ -54,14 +54,24 @@ shinyServer(function(input, output) {
 
       output$kwic <- DT::renderDataTable({
       if (input$go==0) {
-          DT::datatable(data = dataset_bySentence %>%
-                            filter(stringr::str_detect(string = sentence,
-                                                       pattern = stringr::regex(ignore_case = TRUE,
-                                                                                pattern = paste(as.character(tolower(trimws(stringr::str_split(string = exampleTerms, pattern = ",", simplify = TRUE)))), collapse = "|")))) %>%
-                            mutate(Source = paste0("<a target='_blank' href='", link, "'>", title, "</a><br />")) %>%
-                            rename(Sentence = sentence, Date = date) %>%
-                            select(Date, Source, Sentence) %>%
-                            arrange(desc(Date)),
+          temp <- dataset %>%
+              filter(date>input$dateRange[1], date<input$dateRange[2]) %>%
+              filter(stringr::str_detect(string = sentence,
+                                         pattern = stringr::regex(ignore_case = TRUE,
+                                                                  pattern = paste(as.character(tolower(trimws(stringr::str_split(string = input$term, pattern = ",", simplify = TRUE)))), collapse = "|")))) %>%
+              mutate(Source = paste0("<a target='_blank' href='", link, "'>", title, "</a><br />")) %>%
+              rename(Sentence = sentence, Date = date) %>%
+              select(Date, Source, Sentence) %>%
+              arrange(desc(Date))
+
+          if (length(as.character(tolower(trimws(stringr::str_split(string = input$term, pattern = ",", simplify = TRUE)))))==1) {
+              temp$Sentence <- purrr::map_chr(.x = temp$Sentence, .f = function (x) paste(c(rbind(as.character(stringr::str_split(string = x,
+                                                                                                                                  pattern = stringr::regex(pattern = as.character(exampleTerms), ignore_case = TRUE), simplify = TRUE)),
+                                                                                                  c(paste0("<span style='background-color: #FFFF00'>", as.character(str_extract_all(string = x, pattern = regex(as.character(exampleTerms), ignore_case = TRUE), simplify = TRUE)), "</span>"), ""))),
+                                                                                          collapse = ""))
+          }
+          temp
+          DT::datatable(data = temp,
                         options = list(pageLength = 3, lengthMenu = c(3, 5, 10, 15, 20)),
                         escape = FALSE, rownames = FALSE)
       } else {
