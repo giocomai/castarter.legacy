41d1643227e0dd0f6481d810e8ceb0baa90ec1e5
Giorgio Comai <giorgiocomai@gmail.com>
2018-07-29 15:45:19 +0300

    Working UpdateDataset() introduced and documented

--- R/updateDataset.R
+++ R/updateDataset.R
@@ -2,49 +2,145 @@
 #'
 #' Updates a dataset given a previously created parameters export file.
 #'
-#' @param dataset A 'castarter' dataset.
+#' @param dataset A 'castarter' dataset, defaults to NULL. If not given, and project and website are given (possily, through `SetCastarter(project = ..., website =)`), the latest dataset is automatically loaded.
+#' @param links A chacter vector, defaults to NULL. If given, it is checked against the given dataset: new links are downloaded, the correspondent pages extracted, and the result added to the dataset. If not given, the function downloads index pages along the criteria found in the stores param file and continues downloading them as long as there is at least a new link in an index page. Based on this, it then proceeds to update the dataset.
+#' @param maxNumberOfIndexPages An integer, defaults to 1000 to prevent function from running indefinitely (however, this can be set to `Inf`). Maximum number of index pages to be downloaded before the function proceeds with updating the dataset.
+#' @param wait Defaults to 1. Number of seconds to wait between downloading one page and the next. Can be increased to reduce server load, or can be set to 0 when this is not an issue.
+#' @param exportRds Logical, defaults to TRUE. If TRUE, it stores the updated dataset in the Dataset folder in .rds format.
 #' @param project Name of 'castarter' project. Must correspond to the name of a folder in the current working directory. Defaults to NULL, required for importing parameters. This can be left blank if previously set with SetCastarter(project = "project", website = "website").
 #' @param website Name of a website included in a 'castarter' project. Must correspond to the name of a sub-folder of the project folder. Defaults to NULL, required for importing parameters. This can be left blank if previously set with SetCastarter(project = "project", website = "website").
 #' @return A castarter dataset.
 #' @export
 #' @examples
-#' dataset <- UpdateDataset(dataset)
-UpdateDataset <- function(dataset, links = NULL, numberOfIndexPages = 10, wget = FALSE, wait = 1, project = NULL, website = NULL) {
+#' dataset <- UpdateDataset()
+UpdateDataset <- function(dataset = NULL,
+                          links = NULL,
+                          maxNumberOfIndexPages = 1000,
+                          wait = 1,
+                          exportRds = TRUE,
+                          project = NULL,
+                          website = NULL) {
     if (is.null(project) == TRUE) {
         project <- CastarterOptions("project")
     }
     if (is.null(website) == TRUE) {
         website <- CastarterOptions("website")
     }
-    params <- read.table(file = file.path(project, website, "Logs", paste(website, "updateParameters.csv", sep = " - ")), stringsAsFactors = FALSE)
-    params$param[params$param == "NULL"] <- NA
-    params$param[params$param == ""] <- NA
-    params$param[params$param == "NA"] <- NA
-    params$param[params$param == "FALSE"] <- FALSE
-    params$param[params$param == "TRUE"] <- TRUE
+    if (is.null(dataset) == TRUE) {
+        dataset <- LoadDatasets(projectsAndWebsites = paste(project, website, sep = "/"))
+    }
     castarter::CreateFolders(project = project, website = website)
+    paramsFile <- base::file.path(project, website, "Logs", paste(project, website, "parameters.rds", sep = "-"))
+    params <- readRDS(paramsFile)
     if (base::is.null(links)==TRUE) {
-        indexLinks <- CreateLinks(linkFirstChunk = params$param[params$args=="linkFirstChunk"], linkSecondChunk = params$param[params$args=="linkSecondChunk"], startPage = as.integer(params$param[params$args=="startPage"]), endPage = sum(as.integer(params$param[params$args=="startPage"]), numberOfIndexPages), increaseBy = as.integer(params$param[params$args=="increaseBy"]))
-        DownloadContents(links = indexLinks, type = "index", wait = wait)
-        indexHtml <- ImportHtml(from = "index")
-    #    links <- ExtractLinks(domain = params$param[params$args=="domain"], partOfLink = params$param[params$args=="partOfLink"], html = indexHtml, containerType = params$param[params$args=="containerTypeExtractLinks"], containerClass = params$param[params$args=="containerClassExtractLinks"], divClass = params$param[params$args=="divClassExtractLinks"], partOfLinkToExclude = strsplit(params$param[params$args=="partOfLinkToExclude"], "§§§"))
-    }
-    linksNew <- links[is.element(links, dataset$link)==FALSE]
-    previousLength <- length(dataset$link)
-    links <- c(dataset$link, linksNew)
-    DownloadContents(links = links, type = "articles", project = project, website = website, wget = wget, missingPages = TRUE, wait = wait, start = sum(previousLength, 1))
-    DownloadContents(links = links, type = "articles", project = project, website = website, wget = wget, missingPages = FALSE, wait = wait, start = sum(previousLength, 1))
+        if (is.null(params$CreateLinks$dateFormat)) {
 
-    #titles <- ExtractTitles(links = linksNew, method = params$param[params$args=="method_ExtractTitles"], removePunctuation = params$param[params$args=="removePunctuation_ExtractTitles"], onlyStandardCharacters = params$param[params$args=="onlyStandardCharacters_ExtractTitles"], removeString = params$param[params$args=="removeString_ExtractTitles"], removeEverythingAfter = params$param[params$args=="removeEverythingAfter_ExtractTitles"], customXpath = params$param[params$args=="customXpath_ExtractTitles"], maxCharacters = params$param[params$args=="maxCharacters_ExtractTitles"], exportParameters = FALSE)
-    #language_ExtractDates <- params$param[params$args=="language_ExtractDates"]
-    if (is.null(language_ExtractDates) == TRUE) {
-        language_ExtractDates <- Sys.getlocale(category = "LC_TIME")
+            message("Downloading index pages as long as they include links to pages not included in the dataset")
+
+            indexLinks <- CreateLinks(linkFirstChunk = params$CreateLinks$linkFirstChunk,
+                                 linkSecondChunk = params$CreateLinks$linkSecondChunk,
+                                 startPage = params$CreateLinks$startPage,
+                                 endPage = params$CreateLinks$startPage,
+                                 exportParameters = FALSE)
+            DownloadContents(links = indexLinks, type = "index", wait = wait, project = project, website = website)
+            DownloadContents(links = indexLinks, type = "index", wait = wait, project = project, website = website)
+            DownloadContents(links = indexLinks, type = "index", missingPages = FALSE, wait = wait, project = project, website = website)
+            DownloadContents(links = indexLinks, type = "index", missingPages = FALSE, wait = wait, project = project, website = website)
+
+            links <- ExtractLinks(importParameters = TRUE)
+
+            while(sum(is.element(el = links, set = dataset$link))<length(links)&length(indexLinks)<=maxNumberOfIndexPages) {
+                indexLinks <- CreateLinks(linkFirstChunk = params$CreateLinks$linkFirstChunk,
+                                          linkSecondChunk = params$CreateLinks$linkSecondChunk,
+                                          startPage = params$CreateLinks$startPage,
+                                          endPage = params$CreateLinks$startPage+(params$CreateLinks$increaseBy*length(indexLinks)),
+                                          increaseBy = params$CreateLinks$increaseBy,
+                                          exportParameters = FALSE)
+                DownloadContents(links = indexLinks, type = "index", wait = wait, project = project, website = website)
+                DownloadContents(links = indexLinks, type = "index", missingPages = FALSE, wait = wait, project = project, website = website)
+                DownloadContents(links = indexLinks, type = "index", wait = wait, project = project, website = website)
+                DownloadContents(links = indexLinks, type = "index", missingPages = FALSE, wait = wait, project = project, website = website)
+
+                links <- ExtractLinks(importParameters = TRUE,
+                                      id = length(indexLinks))
+
+            }
+            if (length(indexLinks)==maxNumberOfIndexPages) {
+                warning("Download of index pages stopped because maxNumberOfIndexPages has been reached. There may be other new pages that have not been downloaded.")
+            }
+            message("Extracting links to new pages")
+            newLinks <- ExtractLinks(importParameters = TRUE, id = NULL)
+        } else {
+            newLinks <- links
+        }
+        newLinks <- newLinks[!is.element(el = newLinks, set = dataset$link)]
+        allLinks <- c(dataset$link, newLinks)
+        toDownloadL <- is.element(el = allLinks, set = newLinks)
+        message(paste(sum(toDownloadL), "new pages founds."))
+
+        DownloadContents(links = allLinks,
+                         type = "articles",
+                         linksToCheck = toDownloadL,
+                         wait = wait, project = project, website = website)
+
+        DownloadContents(links = allLinks,
+                         type = "articles",
+                         linksToCheck = toDownloadL,
+                         wait = wait,
+                         project = project,
+                         website = website,
+                         missingPages = FALSE)
+
+        DownloadContents(links = allLinks,
+                         type = "articles",
+                         linksToCheck = toDownloadL,
+                         wait = wait,
+                         project = project,
+                         website = website)
+
+        DownloadContents(links = allLinks,
+                         type = "articles",
+                         linksToCheck = toDownloadL,
+                         wait = wait,
+                         project = project,
+                         website = website,
+                         missingPages = FALSE)
     }
-  #  dates <- ExtractDates(dateFormat = params$param[params$args=="dateFormat_ExtractDates"], customString = params$param[params$args=="customString_ExtractDates"], customXpath = params$param[params$args=="customXpath_ExtractDates"], language = language_ExtractDates, keepAllString = params$param[params$args=="keepAllString_ExtractDates"], minDate = params$param[params$args=="minDate"], maxDate = params$param[params$args=="maxDate"], removeEverythingBefore = params$param[params$args=="removeEverythingBefore_ExtractDates"], exportParameters = FALSE)
-    id <- ExtractId(project, website)
+    id <- ExtractId(project = project, website = website)
+    titles <- ExtractTitles(id = id,
+                            importParameters = TRUE,
+                            exportParameters = FALSE,
+                            project = project,
+                            website = website)
+
+    dates <- ExtractDates(id = id,
+                          importParameters = TRUE,
+                          exportParameters = FALSE,
+                          project = project,
+                          website = website)
+
     language <- dataset$language[1]
-    metadata <- ExportMetadata(project = project, website = website, dates = dates, id = id, titles = titles, language = language, links = linksNew)
-   # contents <- ExtractText(metadata = metadata, export = FALSE, maxTitleCharacters = params$param[params$args=="maxTitleCharacters"], removeString = unlist(stringr::str_split(string = params$param[params$args=="removeString_ExtractTxt"], pattern = "§§§")), removeEverythingAfter = params$param[params$args=="removeEverythingAfter_ExtractTxt"], removeEverythingBefore = params$param[params$args=="removeEverythingBefore_ExtractTxt"], removePunctuationInFilename = params$param[params$args=="removePunctuationInFilename"], keepEverything = params$param[params$args=="keepEverything_ExtractTxt"], removeTitleFromTxt = params$param[params$args=="removeTitleFromTxt"], exportParameters = FALSE)
-    dataset <- rbind(dataset, cbind(metadata, contents, stringsAsFactors = FALSE))
+
+    metadata <- ExportMetadata(project = project,
+                               website = website,
+                               dates = dates,
+                               id = id,
+                               titles = titles,
+                               language = language,
+                               links = newLinks)
+
+    text <- ExtractText(id = id,
+                        importParameters = TRUE,
+                        exportParameters = FALSE)
+    newDataset <- ExportDataset(text = text, metadata = metadata, exportRds = FALSE)
+
+    dataset <- dplyr::bind_rows(dataset, newDataset)
+
+    if (exportRds==TRUE) {
+        ExportDataset(dataset = dataset,
+                      exportRds = TRUE,
+                      project = project,
+                      website = website)
+    }
     invisible(dataset)
 }
--- README.md
+++ README.md
@@ -1,57 +1,90 @@
-An introduction to 'castarter' - content analysis starter toolkit for R
+An introduction to ‘castarter’ - content analysis starter toolkit for R
 ================
 Giorgio Comai
-2018-03-22
+2018-07-29
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
-Preliminary note (2018-01-28)
------------------------------
 
-`castarter` is being completely rewritten in order to implement a number of enhancements. As of today, it is again possible to use all key functions for downloading sections of websites, extracting text, and key metadata.
+## Preliminary notes
 
-An example of a script that generates such a dataset is [now available](https://github.com/giocomai/castarter/blob/master/inst/extdata/R-Script-examples/Kremlin_en.R).
+### 2018-07-29
 
-An example Shiny app that allows to explore the dataset thus created is now [part of the package](https://github.com/giocomai/castarter/tree/master/inst/extdata/shiny/DatasetAnalysis) and [can be seen in action online](https://giocomai.shinyapps.io/Kremlin_en/).
+A function that allows to automatically update datasets -
+`updateDataset()` - is now functioning, albeit in a relatively limited
+set of scenarios.
 
-Preliminary note (2018-01-14)
------------------------------
+### 2018-01-28
+
+`castarter` is being completely rewritten in order to implement a number
+of enhancements. As of today, it is again possible to use all key
+functions for downloading sections of websites, extracting text, and key
+metadata.
+
+An example of a script that generates such a dataset is [now
+available](https://github.com/giocomai/castarter/blob/master/inst/extdata/R-Script-examples/Kremlin_en.R).
+
+An example Shiny app that allows to explore the dataset thus created is
+now [part of the
+package](https://github.com/giocomai/castarter/tree/master/inst/extdata/shiny/DatasetAnalysis)
+and [can be seen in action
+online](https://giocomai.shinyapps.io/Kremlin_en/).
+
+### 2018-01-14
 
 The new `castarter` aims to:
 
--   make it possible to run (almost) all of its functions from a web interface (through a Shiny app), allowing to create textual datasets, export them, and conduct basic word frequency analysis virtually without knowing anything about R
--   store parameters used for dataset creation in a format that is machine readbale, making it easy to share the procedure, and allow other users to replicate it
--   automatically generate human-readable log files that detail the procedure
--   have as few dependencies as possible on other packages
--   reducing RAM requirements in order to allow for smooth analysis of relatively big datasets (e.g. &gt;1 million items)
--   facilitate integration with SQL databases
+  - make it possible to run (almost) all of its functions from a web
+    interface (through a Shiny app), allowing to create textual
+    datasets, export them, and conduct basic word frequency analysis
+    virtually without knowing anything about R
+  - store parameters used for dataset creation in a format that is
+    machine readbale, making it easy to share the procedure, and allow
+    other users to replicate it
+  - automatically generate human-readable log files that detail the
+    procedure
+  - have as few dependencies as possible on other packages
+  - reducing RAM requirements in order to allow for smooth analysis of
+    relatively big datasets (e.g. \>1 million items)
+  - facilitate integration with SQL databases
 
-Examples of output
-------------------
+## Examples of output
 
-Some examples of analysis of media contents conducted with `castarter` are available on the author's blog:
+Some examples of analysis of media contents conducted with `castarter`
+are available on the author’s blog:
 
--   [Word frequency of ‘Ukraine’, ‘Crimea’, and ‘Syria’ on Russia’s First Channel](http://www.giorgiocomai.eu/2015/11/03/word-frequency-of-ukraine-crimea-and-syria-on-russias-first-channel/)
--   [Presidents and prime ministers in Romanian media](http://www.giorgiocomai.eu/2016/06/29/presidents-and-prime-ministers-in-romanian-media/)
+  - [Word frequency of ‘Ukraine’, ‘Crimea’, and ‘Syria’ on Russia’s
+    First
+    Channel](http://www.giorgiocomai.eu/2015/11/03/word-frequency-of-ukraine-crimea-and-syria-on-russias-first-channel/)
+  - [Presidents and prime ministers in Romanian
+    media](http://www.giorgiocomai.eu/2016/06/29/presidents-and-prime-ministers-in-romanian-media/)
 
 Interactive examples:
 
--   [Armenia's president website](https://giocomai.shinyapps.io/ArmeniaPresident/)
--   [Official websites of South Caucasus presidents](https://giocomai.shinyapps.io/SouthCaucasusPresidents/)
--   [Kremlin's official website - only time series](https://giocomai.shinyapps.io/kremlinregex/)
+  - [Armenia’s president
+    website](https://giocomai.shinyapps.io/ArmeniaPresident/)
+  - [Official websites of South Caucasus
+    presidents](https://giocomai.shinyapps.io/SouthCaucasusPresidents/)
+  - [Kremlin’s official website - only time
+    series](https://giocomai.shinyapps.io/kremlinregex/)
 
-Installation
-------------
+## Installation
 
-N.B. Please consider that this document illustrates only the most basic parameters of `castarter` functions. More advanced options can be seen by running `?nameOfFunction`.
+N.B. Please consider that this document illustrates only the most basic
+parameters of `castarter` functions. More advanced options can be seen
+by running `?nameOfFunction`.
 
-`castarter` is hosted on GitHub. The easiest way to install it on your system, is to run the following commands in R:
+`castarter` is hosted on GitHub. The easiest way to install it on your
+system, is to run the following commands in R:
 
 ``` r
 install.packages("devtools")
 devtools::install_github("giocomai/castarter")
 ```
 
-For detailed install instructions, including references to packages that need to be preliminary installed on Linux system, as well as advanced setups (including Docker images), see [install instructions](./install.md).
+For detailed install instructions, including references to packages that
+need to be preliminary installed on Linux system, as well as advanced
+setups (including Docker images), see [install
+instructions](./install.md).
 
 Load `castarter` with:
 
@@ -59,25 +92,42 @@ Load `castarter` with:
 library("castarter")
 ```
 
-Using castarter
----------------
+## Using castarter
 
-`castarter` facilitates downloading and extracting textual contents from a given section of a website. `castarter` can be used to analyse one single website, or more websites as part of a project, facilitating comparison among cases. In order to smoothen replication and prevent data losses, by default `castarter` stores a copy of downloaded web pages in html format. To do so, it includes a function that creates the relevant folder structure, and another function that facilitates archiving the html files in compressed format.
+`castarter` facilitates downloading and extracting textual contents from
+a given section of a website. `castarter` can be used to analyse one
+single website, or more websites as part of a project, facilitating
+comparison among cases. In order to smoothen replication and prevent
+data losses, by default `castarter` stores a copy of downloaded web
+pages in html format. To do so, it includes a function that creates the
+relevant folder structure, and another function that facilitates
+archiving the html files in compressed format.
 
-In `castarter` every dataset is expected to be extracted from a website, and each website is expected to be part of a project. This is useful to give a consistent way of storing the materials gathered and to facilitate comparison among cases, but it does not imply additional limitations: it is possible to have projects composed of a single website, and it is possible to make comparisons among websites that are part of different projects.
+In `castarter` every dataset is expected to be extracted from a website,
+and each website is expected to be part of a project. This is useful to
+give a consistent way of storing the materials gathered and to
+facilitate comparison among cases, but it does not imply additional
+limitations: it is possible to have projects composed of a single
+website, and it is possible to make comparisons among websites that are
+part of different projects.
 
 \[Readme to be completed\]
 
-Disclaimer
-----------
+## Disclaimer
 
 `castarter` is under active development:
 
--   some functions may not work as expected;
--   documentation is incomplete (however, most paramaters should be self-descriptive);
--   the code has not been optimised.
+  - some functions may not work as expected;
+  - documentation is incomplete (however, most paramaters should be
+    self-descriptive);
+  - the code has not been optimised.
 
-About the author
-----------------
+## About the author
 
-Giorgio Comai is a researcher at OBCT/CCI (<https://www.balcanicaucaso.org/>) and a data cruncher at EDJNET (<https://www.europeandatajournalism.eu/>). He has completed his PhD at the School of Law and Government, Dublin City University. You can find out more about his work on his [personal website](http://giorgiocomai.eu/). He is on Twitter: <https://twitter.com/giocomai>.
+Giorgio Comai is a researcher at OBCT/CCI
+(<https://www.balcanicaucaso.org/>) and a data cruncher at EDJNET
+(<https://www.europeandatajournalism.eu/>). He has completed his PhD at
+the School of Law and Government, Dublin City University. You can find
+out more about his work on his [personal
+website](http://giorgiocomai.eu/). He is on Twitter:
+<https://twitter.com/giocomai>.
