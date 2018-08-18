6be20c62edfa9faf404fb4fd3ad4d965b035e575
Giorgio Comai <giorgiocomai@gmail.com>
2018-08-18 18:13:19 +0300

    change colnames to enhance output

--- R/summary.R
+++ R/summary.R
@@ -10,12 +10,16 @@
 
 SummariseDataset <- function(dataset) {
     websites <- levels(as.factor(dataset$website))
-    datasetSummary <- data.frame(website = websites, minDates = as.Date(rep(NA, length(websites))), maxDates = as.Date(rep(NA, length(websites))), totalPublications = as.numeric(NA), avgDailyPublications = as.numeric(NA))
+    datasetSummary <- tibble::data_frame(Website = websites,
+                                         From = as.Date(rep(NA, length(websites))),
+                                         Until = as.Date(rep(NA, length(websites))),
+                                         Total = as.numeric(NA),
+                                         `Average daily` = as.numeric(NA))
     for (i in seq_along(websites)) {
-        datasetSummary$minDates[i] <- min(dataset$date[dataset$website==websites[i]])
-        datasetSummary$maxDates[i] <- max(dataset$date[dataset$website==websites[i]])
-        datasetSummary$totalPublications[i] <- nrow(dataset[dataset$website==websites[i],])
-        datasetSummary$avgDailyPublications[i] <- datasetSummary$totalPublications[i]/as.numeric(diff.Date(x = c(datasetSummary$minDates[i], datasetSummary$maxDates[i])))
+        datasetSummary$From[i] <- min(dataset$date[dataset$website==websites[i]])
+        datasetSummary$Until[i] <- max(dataset$date[dataset$website==websites[i]])
+        datasetSummary$Total[i] <- nrow(dataset[dataset$website==websites[i],])
+        datasetSummary$`Average daily`[i] <- datasetSummary$Total[i]/as.numeric(diff.Date(x = c(datasetSummary$From[i], datasetSummary$Until[i])))
     }
     return(datasetSummary)
 }
